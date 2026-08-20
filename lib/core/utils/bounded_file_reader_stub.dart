643b2cf04951ef8a0d7100774415958ab3c20726
Sanskar <sanskarin@outlook.in>
2026-08-20 06:37:09 +0530

    fix: surface web path errors asynchronously

--- lib/core/utils/bounded_file_reader_stub.dart
+++ lib/core/utils/bounded_file_reader_stub.dart
@@ -5,7 +5,7 @@ import 'package:notenest/core/errors/app_exception.dart';
 Future<Uint8List> readBoundedFile(
   String path, {
   required void Function(int byteLength) validateLength,
-}) {
+}) async {
   throw const ImportExportException(
     'Filesystem path reading is unavailable on this platform.',
   );
